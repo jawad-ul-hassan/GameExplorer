a120cfd77ff2c5f96bb6ea9df0c46dfe16975d6e
jawad-ul-hassan <intelligentjawad@gmail.com>
2023-06-07 17:20:27 +0500

    routes add with error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,30 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <Navbar />
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        rowGap="25px"
+        padding="24"
+      >
+        <Heading fontSize="7xl">Oops!</Heading>
+        <Text fontSize="2xl">
+          {isRouteErrorResponse(error)
+            ? 'This page does not exist.'
+            : 'An expected error occured.'}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,22 @@
 import { Flex, Grid, GridItem, Show } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
-import GameGrid from './components/GameGrid';
-import GenreList from './components/GenreList';
-import PlatformSelector from './components/PlatformSelector';
-import SortSelector from './components/SortSelector';
-import GameHeading from './components/GameHeading';
+import GameGrid from '../components/GameGrid';
+import GenreList from '../components/GenreList';
+import PlatformSelector from '../components/PlatformSelector';
+import SortSelector from '../components/SortSelector';
+import GameHeading from '../components/GameHeading';
 
-const App = () => {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" " main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `'main'`,
+        lg: `'aside main'`,
       }}
       templateColumns={{
         base: '1fr',
         lg: '250px 1fr',
       }}
     >
-      <GridItem area="nav">
-        <Navbar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -38,4 +34,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default HomePage;
